4f7e1661ffb7d3576b74ac49b2b0a4a026a5a17d
kresnafaza19 <kresnafaza19@gmail.com>
2021-03-21 11:03:22 +0700

    class

--- src/Program.cs
+++ src/Program.cs
@@ -5,41 +5,66 @@ using System.Collections.Generic;
 
 namespace Tubes_2
 {
+    class graph {
+        public List<node> nodes;
+
+        public graph(){
+            this.nodes = new List<node>();
+        }
+
+        public void addNode(node x){
+            if (!this.nodes.Contains(x)){
+                this.nodes.Add(x);
+            }
+        }
+        public void Info(){
+            foreach(node i in this.nodes){
+                Console.WriteLine(i.vertex);
+            }
+        }
+        
+
+    }
     class node {
         public string vertex;
-        public List<graph> adjacent;
+        public List<node> adjacent;
 
         public node(string v){
             this.vertex = v;
-            this.adjacent = new List<graph>();
+            this.adjacent = new List<node>();
         }
 
         public void printInfo(){
-            Console.WriteLine("Vertices = {0}",this.vertices);
+            Console.WriteLine("Vertices : {0}",this.vertex);
             if (this.adjacent.Count == 0){
-                Console.WriteLine("kosong");
+                Console.WriteLine("Adjacent : Kosong");
+            }
+            else {
+                Console.WriteLine("Adjacent : ");
+                foreach(node i in this.adjacent){
+                    Console.WriteLine(i.vertex);
+                }
             }
         }
 
-        public addAdj(node x){
+        public void addAdj(node x){
             this.adjacent.Add(x);
-            return this;
+            x.adjacent.Add(this);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            // List<int> coba = new List<int>();
-            // coba.Add(1);
-            // coba.Add(2);
-            // coba.Add(3);
-            // foreach (int i in coba){
-            //     Console.WriteLine("Elemen = {0}",i);
-            // }
             node tes = new node("A");
             node temp = new node("B");
+            graph a = new graph();
+            a.addNode(tes);
+            a.addNode(temp);
+            a.addNode(tes);
+            a.Info();
             tes.addAdj(temp);
+            temp.printInfo();
             tes.printInfo();
         }
     }
